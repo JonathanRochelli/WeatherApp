2c78ac1bf0d694efee4f7450d6b50dcf9bdaa98a
Jonathan Rochelli <jonathan.rochelli@gmail.com>
2021-08-19 15:34:56 +0200

    Get gps coordinates from API gouv

---
+++ src/app/geolocation.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
+import { Point } from './point';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GeolocationService {
+
+  constructor(private http : HttpClient) { }
+
+  getCoordinates(city  : string) : Observable<Point> {
+    return this.http.get<Point>("https://api-adresse.data.gouv.fr/search", { 
+      params : new HttpParams()
+        .set('q', city)
+    }).pipe( map( (response : any) => { return  response.features[0].geometry }))
+  }
+}
